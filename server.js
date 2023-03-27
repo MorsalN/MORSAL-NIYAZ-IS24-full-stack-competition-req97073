17d06c3c37ab6293705912944e48120d57c57e25
Morsal Niyaz <morsalniyaz@gmail.com>
2023-03-27 13:24:17 -0700

    added products list, generted main table and created a details column

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const express = require('express');
 const path = require('path');
 
+// const product_data = require('.product_data'); // store and retrieve product data
+
 const app = express();
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('./swagger.json');
@@ -15,6 +17,7 @@ const PORT = 3000;
 //  Use
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // to let express know all static folders are in public
+app.set('views', path.join(__dirname, 'views'));
 // app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
@@ -24,7 +27,7 @@ app.set('view engine', 'ejs');
 
 
 // Pre-populating JSON object with 40 sample products using Chance as a random generator
-const products = []; // create an empty array to store products
+const productsDatabase = []; // create an empty array to store products
 
 for (let i = 1; i <= 40; i++) { // loop through 40 times to generate 40 products
   const product = {
@@ -40,20 +43,40 @@ for (let i = 1; i <= 40; i++) { // loop through 40 times to generate 40 products
     ],
     scrumMasterName: chance.name(),
     startDate: chance.date({ string: true, american: false }),
-    methodology: chance.pickone(['Agile', 'Waterfall', 'Scrum'])
+    methodology: chance.pickone(['Agile', 'Waterfall'])
   };
-  products.push(product); // add the generated product to the products array
+  productsDatabase.push(product); // add the generated product to the products array
 }
 
 
+// GET
 // Home Endpoint
 app.get('/api', (req, res) => {
-  res.render('pages/index');
-  res.status(200);
+  res.redirect('/api/products');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// Products Endpoint
+app.get('/api/products', (req, res) => {
+  // console.log('productDatabaseArray', productsDatabase);
+  const templateVars = {
+    productList: productsDatabase
+  };
+  res.render('pages/index', templateVars);
+});
+
+// View Details Product Endpoint
+app.get('/api/products/:id/details', (req, res) => {
+  const productId = req.params.id;
+  const product = productsDatabase.find(p => p.productId == productId);
+  const templateVars = {
+    product: product
+  };
+  res.render('pages/details', templateVars);
+});
+
+// Health Endpoint 
+app.get("/api/health", (req, res) => {
+  res.status(200).send('Component is healthy! :)');
 });
 
 
