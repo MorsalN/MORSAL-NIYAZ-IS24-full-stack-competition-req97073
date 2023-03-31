16294cbdf6a68f1c76676f8566b17e5d54f1e43c
Morsal Niyaz <morsalniyaz@gmail.com>
2023-03-31 08:32:05 -0700

    better error handling, ui updates and clean up

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = 3000;
 
 
-//  Use
+//  Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // to let express know all static folders are in public
 // Enable CORS for all routes
@@ -30,8 +30,7 @@ const jsonString = fs.readFileSync('generated-data.json', 'utf8');
 const productsDatabase = JSON.parse(jsonString);
 const productCount = Object.keys(productsDatabase).length;
 
-// Set 
-// What engine to use (View) and extention files to look at
+// Set View Engine and Views Path
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -63,73 +62,101 @@ for (let i = 1; i <= 40; i++) {
 // GET
 // Home Endpoint
 app.get('/api', (req, res) => {
-  res.redirect('/api/products');
+  try {
+    res.redirect('/api/products');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Products Endpoint
 app.get('/api/products', (req, res) => {
-
-  // Using template for variables to render on the index page
-  const templateVars = {
-    productList: productsDatabase,
-    productCount: productCount
-  };
-  res.status(200).render('pages/index', templateVars);
+  try {
+    // Using template for variables to render on the index page
+    const templateVars = {
+      productList: productsDatabase,
+      productCount: productCount
+    };
+    res.status(200).render('pages/index', templateVars);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Add Products Endpoint
 app.get('/api/products/add', (req, res) => {
-  const templateVars = {
-    productList: productsDatabase,
-    productCount: productCount
-  };
-  res.status(200).render('pages/product_add', templateVars);
+  try {
+    const templateVars = {
+      productList: productsDatabase,
+      productCount: productCount
+    };
+    res.status(200).render('pages/product_add', templateVars);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // View Details Product Endpoint
 app.get('/api/products/:id/details', (req, res) => {
-
-  // Turning id from the request from string to integer 
-  const productId = parseInt(req.params.id);
-  const product = productsDatabase[productId];
-  if (!product) {
-    res.status(404).send('Product not found');
-  } else {
-    const templateVars = {
-      productId: productId,
-      productName: product.productName,
-      productOwnerName: product.productOwnerName,
-      Developers: product.Developers,
-      scrumMasterName: product.scrumMasterName,
-      startDate: product.startDate,
-      methodology: product.methodology
-    };
-    res.status(200).render('pages/product_details', templateVars);
+  try {
+    // Turning id from the request from string to integer 
+    const productId = parseInt(req.params.id);
+    const product = productsDatabase[productId];
+    if (!product) {
+      res.status(404).send('Product not found');
+    } else {
+      const templateVars = {
+        productId: productId,
+        productName: product.productName,
+        productOwnerName: product.productOwnerName,
+        Developers: product.Developers,
+        scrumMasterName: product.scrumMasterName,
+        startDate: product.startDate,
+        methodology: product.methodology
+      };
+      res.status(200).render('pages/product_details', templateVars);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 // Products Count Endpoint
 app.get('/api/products/count', (req, res) => {
-  res.status(200).json({ count: productCount });
+  try {
+    res.status(200).json({ count: productCount });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Edit Product Endpoint
 app.get('/api/products/:id/edit', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = productsDatabase[productId];
-  if (!product) {
-    res.status(404).send('Product not found');
-  } else {
-    const templateVars = {
-      productId: productId,
-      productName: product.productName,
-      productOwnerName: product.productOwnerName,
-      Developers: product.Developers,
-      scrumMasterName: product.scrumMasterName,
-      startDate: product.startDate,
-      methodology: product.methodology
-    };
-    res.status(200).render('pages/product_edit', templateVars);
+  try {
+    const productId = parseInt(req.params.id);
+    const product = productsDatabase[productId];
+    if (!product) {
+      res.status(404).send('Product not found');
+    } else {
+      const templateVars = {
+        productId: productId,
+        productName: product.productName,
+        productOwnerName: product.productOwnerName,
+        Developers: product.Developers,
+        scrumMasterName: product.scrumMasterName,
+        startDate: product.startDate,
+        methodology: product.methodology
+      };
+      res.status(200).render('pages/product_edit', templateVars);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
@@ -142,58 +169,73 @@ app.get("/api/health", (req, res) => {
 // POST
 // Add Products Endpoint
 app.post('/api/products/add', (req, res) => {
-  const { Developers, productName, productOwnerName, scrumMasterName, startDate, methodology } = req.body;
+  try {
+    const { Developers, productName, productOwnerName, scrumMasterName, startDate, methodology } = req.body;
 
-  // Ensure ids are not the same
-  const newProductId = productCount + 1;
+    // Ensure ids are not the same
+    const newProductId = productCount + 1;
 
-  // Add a new product to the existing data
-  const newProductData = {
-    productId: newProductId,
-    productName,
-    productOwnerName,
-    Developers,
-    scrumMasterName,
-    startDate,
-    methodology
-  };
-  productsDatabase[newProductId] = newProductData;
+    // Add a new product to the existing data
+    const newProductData = {
+      productId: newProductId,
+      productName,
+      productOwnerName,
+      Developers,
+      scrumMasterName,
+      startDate,
+      methodology
+    };
+    productsDatabase[newProductId] = newProductData;
 
-  // Write the updated data back to the file
-  fs.writeFileSync('generated-data.json', JSON.stringify(productsDatabase));
+    // Write the updated data back to the file
+    fs.writeFileSync('generated-data.json', JSON.stringify(productsDatabase));
 
-  res.status(201).redirect('/api/products');
+    res.status(201).redirect('/api/products');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Edit Product Endpoint
 app.post('/api/products/:id/edit', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const { Developers, productName, productOwnerName, scrumMasterName, startDate, methodology } = req.body;
+  try {
+    const productId = parseInt(req.params.id);
+    const { Developers, productName, productOwnerName, scrumMasterName, startDate, methodology } = req.body;
 
-  // Update the existing product data
-  productsDatabase[productId].productName = productName;
-  productsDatabase[productId].productOwnerName = productOwnerName;
-  productsDatabase[productId].Developers = Developers;
-  productsDatabase[productId].scrumMasterName = scrumMasterName;
-  productsDatabase[productId].startDate = startDate;
-  productsDatabase[productId].methodology = methodology;
+    // Update the existing product data
+    productsDatabase[productId].productName = productName;
+    productsDatabase[productId].productOwnerName = productOwnerName;
+    productsDatabase[productId].Developers = Developers;
+    productsDatabase[productId].scrumMasterName = scrumMasterName;
+    productsDatabase[productId].startDate = startDate;
+    productsDatabase[productId].methodology = methodology;
 
-  // Write the updated data back to the file
-  fs.writeFileSync('generated-data.json', JSON.stringify(productsDatabase));
+    // Write the updated data back to the file
+    fs.writeFileSync('generated-data.json', JSON.stringify(productsDatabase));
 
-  res.status(200).redirect('/api/products');
+    res.status(200).redirect('/api/products');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // DELETE
 // Delete Product Endpoint
 app.post("/api/products/:id/delete", (req, res) => {
-  const productId = parseInt(req.params.id);
-  delete productsDatabase[productId];
+  try {
+    const productId = parseInt(req.params.id);
+    delete productsDatabase[productId];
 
-  // Write the updated data back to the file
-  fs.writeFileSync('generated-data.json', JSON.stringify(productsDatabase));
+    // Write the updated data back to the file
+    fs.writeFileSync('generated-data.json', JSON.stringify(productsDatabase));
 
-  res.status(200).redirect('/api/products');
+    res.status(200).redirect('/api/products');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // PORT
